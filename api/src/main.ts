b0866534e68ef5db3474aad4c10f51063f37806d
Thang X. Vu <thang@dedot.dev>
2025-12-14 10:01:19 +0700

    add morgan

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
 
   app.useGlobalPipes(
     new ValidationPipe({
