33660189ec803af58ac3494a9a9b08613e810e79
Thang X. Vu <thang@dedot.dev>
2025-12-18 09:44:29 +0700

    integrate gluestack-ui

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Provider as JotaiProvider } from 'jotai';
@@ -8,34 +7,34 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
   throw new Error('Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in environment variables');
 }
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ClerkLoaded>
-        <JotaiProvider>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode={colorScheme || 'system'}>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <ClerkLoaded>
+          <JotaiProvider>
             <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="videos/[id]" options={{ title: 'Video Details', headerBackButtonDisplayMode: 'minimal' }} />
-              <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+              <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+              <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+              <Stack.Screen name="videos/[id]"
+                            options={{title: 'Video Details', headerBackButtonDisplayMode: 'minimal'}}/>
+              <Stack.Screen name="modal" options={{presentation: 'modal', title: 'Modal'}}/>
             </Stack>
-            <StatusBar style="auto" />
-          </ThemeProvider>
-        </JotaiProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+            <StatusBar style="auto"/>
+          </JotaiProvider>
+        </ClerkLoaded>
+      </ClerkProvider>
+    </GluestackUIProvider>
   );
 }
